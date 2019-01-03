e413e3b648e0ad5a2e295106a09a265798ad8035
ralejandrormz <ralejandrormz@gmail.com>
2019-01-03 17:18:47 -0600

    Recurso seguro incompleto 2-1-2018

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name = "Rafael";
+  nameLower = "rafael alejandro manzo ramírez";
+  arr:number[] = [1,2,3,4,5,6,7,8,9,10];
+  PI:number = Math.PI;
+  a:number = 0.234; 
+  salary:number = 500.01; 
+
+  heroe = {
+    nombre : "Logan",
+    clave : "Wolverine",
+    edad : "500",
+    direccion : {
+      calle : "Calle X",
+      numero : "1000",
+      ciudad : "X",
+      estado : "USA"
+    },
+    habilidad : "Regeneración",
+  }
+
+  promiseValue = new Promise( (resolve, reject) => {
+    
+    setTimeout( ()=>resolve("Llego la data"), 3500 )
+  })
+
+  date  = new Date();
+
+  video = "ixm2KvrnmCk";
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import localEs from '@angular/common/locales/es'
+import { registerLocaleData} from '@angular/common'
+
+import { CapitalizedPipe } from './pipes/capitalized.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+
+
+registerLocaleData(localEs);
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizedPipe,
+    DomseguroPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { 
+      provide : LOCALE_ID,
+      useValue : "es"  
+    }
+
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
